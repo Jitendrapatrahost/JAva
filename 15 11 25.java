367ed270f89e0f64c645507efab81307a082737b
Jitendrapatrahost <jitendrapatra.host@gmail.com>
2025-11-15 14:43:12 +0530

    Update 15 11 25.java

--- 15 11 25.java
+++ 15 11 25.java
@@ -4,7 +4,10 @@ class Bank {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter your Account Balance:");
         String balInput = sc.nextLine();
+
+        System.out.println("Enter Withdrawal Amount:");
         String wInput = sc.nextLine();
 
         try {
@@ -12,17 +15,19 @@ class Bank {
             int withdraw = Integer.parseInt(wInput);
 
             if (withdraw > balance)
-                throw new ArithmeticException("Insufficient Balance");
+                throw new ArithmeticException("Error: Insufficient Balance");
 
             if (withdraw <= 0)
-                throw new Exception("Invalid Withdrawal Amount");
+                throw new Exception("Error: Withdrawal amount must be greater than zero");
 
             balance = balance - withdraw;
+
             System.out.println("Withdrawal Successful");
-            System.out.println("Updated Balance: " + balance);
+            System.out.println("Amount Withdrawn: " + withdraw);
+            System.out.println("Remaining Balance: " + balance);
         }
         catch(NumberFormatException e) {
-            System.out.println("Invalid Number");
+            System.out.println("Error: Please enter only numbers.");
         }
         catch(ArithmeticException e) {
             System.out.println(e.getMessage());
